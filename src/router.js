51ff3c394babbd56ed62a677f93aaebf1fcd95e5
You <you85630@foxmail.com>
2019-05-23 14:53:35 +0800

    init

--- src/router.js
+++ src/router.js
@@ -3,8 +3,6 @@ import Router from 'vue-router'
 import VueCookie from 'vue-cookies'
 Vue.use(Router)
 
-// keepAlive: (false) 设为true后页面在切换标签后不会缓存，如果需要缓存，无需设置这个字段，而且需要设置页面组件name属性和路由配置的name一致
-
 const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
@@ -19,7 +17,7 @@ const router = new Router({
 
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title
-  let login = JSON.parse(VueCookie.get('token'))
+  let login = VueCookie.get('token')
   if (to.meta.requireAuth) {
     if (!login) {
       next({
